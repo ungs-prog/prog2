35e6672facae98e66d35bf0d59d1d02d34fa49a1
Dato Simó <dato@net.com.org.es>
2016-04-30 20:14:29 -0300

    Fontify code.

--- bin/install-org.el
+++ bin/install-org.el
@@ -1,7 +1,9 @@
 (require 'package)
-(setq package-archives '(("org" . "http://orgmode.org/elpa/")))
+(setq package-archives '(("org" . "http://orgmode.org/elpa/")
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 (package-refresh-contents)
 
 ;; TODO: do not reinstall every time.
 (package-install 'org)
+(package-install 'htmlize)
